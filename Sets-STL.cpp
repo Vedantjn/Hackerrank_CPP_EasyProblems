4c79b1467dc3d1195d44db1cdf49963f09b015a1
Vedantjn <76029713+Vedantjn@users.noreply.github.com>
2021-06-09 10:36:04 +0530

    Update Sets-STL.cpp

--- Sets-STL.cpp
+++ Sets-STL.cpp
@@ -3,41 +3,39 @@
 #include <vector>
 #include <iostream>
 #include <set>
-#include <map>
 #include <algorithm>
 using namespace std;
 
 int main()
 {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    map<string, int> m;
-    int N = 0;
+    set<int> s;
+    int N = 0, i;
     cin >> N;
-    for (int i = 0; i < N; i++)
+    for (i = 0; i < N; i++)
     {
-        int q = 0;
+        int q = 0, x = 0;
         cin >> q;
-        string x;
+        cin >> x;
         if (q == 1)
         {
-            int y = 0;
-            cin >> x;
-            cin >> y;
-            m[x] += y;
+            s.insert(x);
         }
         else if (q == 2)
         {
-            cin >> x;
-            m.erase(x);
+            s.erase(x);
         }
         else
         {
-            cin >> x;
-            map<string, int>::iterator itr = m.find(x);
-            if (itr != m.end())
-                cout << m[x] << endl;
+            set<int>::iterator itr = s.find(x);
+            if (itr == s.end())
+            {
+                cout << "No" << endl;
+            }
             else
-                cout << 0 << endl;
+            {
+                cout << "Yes" << endl;
+            }
         }
     }
     return 0;
